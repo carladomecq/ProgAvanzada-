e3d94f1cc38c49ef5a2af3bffb9a2fd5ded9de3c
Carla Domecq <67656024+carladomecq@users.noreply.github.com>
2023-04-16 17:16:29 -0300

    Update funciones.py

--- funciones.py
+++ funciones.py
@@ -1,2 +1,34 @@
-def perimetro_cuadrado(l):
-    return l*4
+############################################################
+# Calculamos el area de las diferentes figuras geometricas #
+############################################################
+def area_cuadrado(lado1):
+    return lado1*lado1
+
+def area_rectangulo(lado1, lado2):
+    return lado1 * lado2
+
+def area_triangulo(base, altura):
+    return (base*altura)/2
+
+import math #importamos math module para usar la constante pi
+def area_circulo(radio):
+    return math.pi * radio * radio
+
+############################################################
+#Calculamos perimetro de las diferentes figuras geometricas# (
+############################################################
+
+
+def perimetro_cuadrado(lado1):
+    return lado1*4
+
+def perimetro_rectangulos(Lado1, lado2):
+    return 2*(Lado1 + lado2)
+
+def perimetro_triangulo(Lado1, lado2, lado3):
+    return (Lado1 + lado2 + lado3)
+
+import math #importamos math module para usar la constante pi
+def perimetro_circulo(radio):
+    return 2* math.pi * radio 
+
